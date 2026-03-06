b7a9861c9764698d127a905b72ef6efd2023f663
stasoplay <ramovstanislav@gmail.com>
2026-03-06 16:01:55 +0700

    Add JS patricles

---
+++ particles.js
@@ -0,0 +1,31 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 100 },
+    "color": { "value": "#454a5c" },
+    "shape": { "type": "edge" },
+    "opacity": { "value": 0.5 },
+    "size": { "value": 3 },
+    "line_linked": { "enable": false },
+    "move": { "enable": true, "speed": 3 }
+  },
+
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+    },
+    "modes": {
+      "bubble": {
+        "distance": 200,
+        "size": 20,
+        "duration": 2,
+        "opacity": 0.8,
+        "speed": 3
+      },
+    }
+  },
+  "retina_detect": true
+});
