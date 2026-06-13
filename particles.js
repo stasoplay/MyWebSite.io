f70ea676d115100ae678fb92eb22436e4ed4de92
Ramov <ramovstanislav@gmail.com>
2026-06-13 19:53:17 +0700

    Addin portfolio

--- particles.js
+++ particles.js
@@ -1,31 +1,36 @@
-particlesJS("particles-js", {
-  "particles": {
-    "number": { "value": 100 },
-    "color": { "value": "#454a5c" },
-    "shape": { "type": "edge" },
-    "opacity": { "value": 0.5 },
-    "size": { "value": 3 },
-    "line_linked": { "enable": false },
-    "move": { "enable": true, "speed": 3 }
-  },
-
-  "interactivity": {
-    "detect_on": "window",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "bubble"
-      },
+window.onload = function() {
+  particlesJS("particles-js", {
+    "particles": {
+      "number": { "value": 100 },
+      "color": { "value": "#454a5c" },
+      "shape": { "type": "circle" },
+      "opacity": { "value": 0.5 },
+      "size": { "value": 3 },
+      "line_linked": { "enable": false },
+      "move": { "enable": true, "speed": 1 }
     },
-    "modes": {
-      "bubble": {
-        "distance": 200,
-        "size": 20,
-        "duration": 2,
-        "opacity": 0.8,
-        "speed": 3
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
       },
-    }
-  },
-  "retina_detect": true
-});
+      "modes": {
+        "bubble": {
+          "distance": 400,
+          "size": 20,
+          "duration": 2,
+          "opacity": 0.8,
+          "speed": 3
+        },
+      }
+    },
+    "retina_detect": true
+  });
+};
